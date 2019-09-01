915f8407833bc890c42a9fae6a8d42af366bb87b
lakshyajit <lakshyajit165@gmail.com>
2019-09-01 15:23:31 +0530

    Passing data to a page and printing it using HttpServlet

--- src/main/java/com/stackroute/SpringWebApp/HomeController.java
+++ src/main/java/com/stackroute/SpringWebApp/HomeController.java
@@ -3,12 +3,23 @@ package com.stackroute.SpringWebApp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(){
-        System.out.println("Inside home!");
+    public String home(HttpServletRequest req){
+
+        HttpSession session = req.getSession();
+
+        String name = req.getParameter("name");
+
+        System.out.println("Hi!"+name);
+
+        session.setAttribute("name", name);
+
         return "home";
     }
 }
--- src/main/webapp/pages/home.jsp
+++ src/main/webapp/pages/home.jsp
@@ -6,5 +6,6 @@
     </head>
     <body>
         <h1>Home Page</h1>
+        <h2>Hello ${name}</h2>
     </body>
 </html>
